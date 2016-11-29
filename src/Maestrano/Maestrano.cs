6109f4e3f6b45303bf1bcd585aae9619790307aa
alachaum <arnaud.lachaume@maestrano.com>
2016-11-29 18:07:03 +1100

    update version

--- src/Maestrano/Maestrano.cs
+++ src/Maestrano/Maestrano.cs
@@ -21,7 +21,7 @@ namespace Maestrano
     public static class MnoHelper
     {
         // VERSION
-        public static string Version { get { return "0.15.3"; } }
+        public static string Version { get { return "1.0.0"; } }
 
         private static Dictionary<string, Preset> presetDict;
         private static Preset defaultPreset;
